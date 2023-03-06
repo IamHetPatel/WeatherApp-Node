dd70bf5563fe31f0e6017fe4dba9a79f34cf2a27
Het Patel <hetp943@gmail.com>
2023-03-06 08:33:30 +0530

    backend half

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Created a website which gives the weather of specified cities at the current time using NodeJS and weather API.
 
-Just a basic implementation of NodeJS.# WeatherApp-Node
+Just a basic implementation of NodeJS.
+# WeatherApp-Node
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const homeFile = fs.readFileSync("home.html","utf-8");
+var requests = require("requests");
 
-const server= http.createServer((req,res)=> {
+const homeFile = fs.readFileSync("index.html", "utf-8");
 
-})
+const replaceVal = (tempVal,orgVal) =>{
+  let temperature = tempVal.replace("{%tempVal%}",orgVal.main.temp)
+   temperature = tempVal.replace("{%tempMax%}",orgVal.main.temp_max)
+   temperature = tempVal.replace("{%tempMin%}",orgVal.main.temp_min)
+   temperature = tempVal.replace("{%location%}",orgVal.name)
+   temperature = tempVal.replace("{%country%}",orgVal.sys.country)
+
+   return temperature;
+}
+const server = http.createServer((req, res) => {
+  if ((req.url = "/")) {
+    requests("https://api.openweathermap.org/data/2.5/weather?q=VADODARA&appid=be9c1e2379822222620543461e2b4d4e")
+      .on("data", function (chunk) {
+        const objData = JSON.parse(chunk);
+        const arrayData = [objData];
+        // console.log(arrayData[0].main.temp);
+        const realTimeData = arrayData.map((val) => replaceVal(homeFile,val));
+          res.write(realTimeData);
+        })
+        .on("end", function (err) {
+            if (err) return console.log("connection closed due to errors", err);
+            res.end();
+        });
+   
+  }
+});
+
+server.listen(5500,"127.0.0.1");
